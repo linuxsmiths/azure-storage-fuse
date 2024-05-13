f82386d72169bb38728bc0df37c182f1af1a9462
Nagendra Tomar <natomar@microsoft.com>
2024-05-13 06:26:19 +0000

    Add initial structures needed to mount the filesystem

---
+++ nfs/inc/nfs_api_context.h
@@ -0,0 +1,325 @@
+#pragma once
+#include "nfs_client.h"
+#include "fuse_optype.h"
+
+class NfsApiContext
+{
+private:
+
+    // The client for which the context is created.
+    // TODO: Since our client is a singleton class, see if we really need to hold this info here.
+    NfsClient* client;
+
+    // Fuse request structure.
+    // This will be the request structure passed from the fuse layer.
+    fuse_req* req;
+
+    // Max number of times the NFS APIs can be retried.
+    static int maxErrnoRetries;
+
+    int numOfTimesRetried;
+
+
+protected:
+    // Operation type. This is used only for logging.
+    enum fuse_optype optype;
+
+public:
+
+    NfsApiContext(NfsClient* _client, struct fuse_req* _req, enum fuse_optype _optype):
+        client(_client),
+        req(_req),
+        numOfTimesRetried(0),
+        optype(_optype)
+    {}
+
+    virtual ~NfsApiContext() {};
+
+    static void setMaxErrnoRetries(int maxRetries)
+    {
+        maxErrnoRetries = maxRetries;
+    }
+
+    static int getMaxErrnoRetries()
+    {
+        return maxErrnoRetries;
+    }
+
+    struct nfs_context* GetNfsContext()
+    {
+        return client->GetNfsContext();
+    }
+
+    // This method will reply with error and delete the context object.
+    void replyError(int rc) {
+        if (rc) {
+            //logError(rc);
+        }
+
+#if 0
+        client_->getLogger()->LOG_MSG(
+            LOG_DEBUG, "%s(%lu): %d\n", __func__, fuse_get_unique(req_), rc);
+#endif
+
+        fuse_reply_err(req, rc);
+        delete this;
+    }
+
+
+    /// @brief Check RPC completion for success.
+    ///
+    /// On failure, retry is set true if the error is
+    /// retryable; on non-retryable error, the FUSE request
+    /// is completed in error and as a side effect, the
+    /// RpcContext object is destroyed.
+    bool succeeded(
+        int rpc_status,
+        int nfs_status,
+        bool& retry,
+        bool idempotent = true)
+    {
+
+        retry = false;
+        if (rpc_status != RPC_STATUS_SUCCESS && (numOfTimesRetried < getMaxErrnoRetries()))
+        {
+
+#if 0
+            client_->getLogger()->LOG_MSG(
+                LOG_WARNING,
+                "%s: RPC status %d (%lu).\n",
+                conn_->describe().c_str(),
+                rpc_status,
+                fuse_get_unique(req_));
+#endif
+
+            // TODO: Should we check here only if the API has exhausted the max possible retries??
+            retry = true;
+            return false;
+        }
+
+#if 0
+        if (client_->errorInjection() && idempotent && (numOfTimesRetried < getMaxErrnoRetries())) {
+            client_->getLogger()->LOG_MSG(
+                LOG_DEBUG,
+                "%s: simulating failure for %lu.\n",
+                __func__,
+                fuse_get_unique(req_));
+            failConnection();
+            retry = true;
+            return false;
+        }
+
+        if (client_->errorInjection() && idempotent) {
+            // TODO Inject more random errors
+            nfs_status = NFS3ERR_ROFS;
+        }
+#endif
+
+        if (nfs_status != NFS3_OK)
+        {
+            if (idempotent && (numOfTimesRetried < getMaxErrnoRetries()) && isRetryableError(nfs_status))
+            {
+                numOfTimesRetried++;
+
+#if 0
+                client_->getLogger()->LOG_MSG(
+                    LOG_INFO,
+                    "%s: Retrying request %lu (attempt %u/%u).\n",
+                    __func__,
+                    fuse_get_unique(req_),
+                    numOfTimesRetried,
+                    getMaxErrnoRetries());
+#endif
+
+                retry = true;
+                return false;
+            }
+            else
+            {
+                if (idempotent && numOfTimesRetried >= getMaxErrnoRetries())
+                {
+#if 0
+                    client_->getLogger()->LOG_MSG(
+                        LOG_INFO,
+                        "%s: Max retry attempts reached, failing operation (%u/%u).\n",
+                        __func__,
+                        numOfTimesRetried,
+                        getMaxErrnoRetries());
+#endif
+                }
+                else if (idempotent && !isRetryableError(nfs_status))
+                {
+#if 0
+                    client_->getLogger()->LOG_MSG(
+                        LOG_INFO,
+                        "%s: Error #%d is not retryable.\n",
+                        __func__,
+                        nfs_status);
+#endif
+                }
+            }
+
+            // This will send an error response and will delete the context object.
+            // hence the caller should not be accessing this object after this point.
+            // TODO: See if this function should be offloaded to the caller.
+            replyError(-nfsstat3_to_errno(nfs_status));
+            return false; // error occurred.
+        }
+
+        return true; // success.
+    }
+
+    bool isRetry() const
+    {
+        return numOfTimesRetried > 0;
+    }
+
+    bool isRetryableError(int nfs_status)
+    {
+        switch (nfs_status)
+        {
+        case NFS3ERR_IO:
+        case NFS3ERR_SERVERFAULT:
+        case NFS3ERR_ROFS:
+        case NFS3ERR_PERM:
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    // This should also contain all the methods needed to send reply back to the client as we use low level fuse API calls.
+    // Just adding the write method now, other methods should be added.
+    void replyWrite(size_t count)
+    {
+        fuse_reply_write(req, count);
+        delete this;
+    }
+
+    void replyEntry(const struct fuse_entry_param* e)
+    {
+
+        //client_->getLogger()->LOG_MSG(
+        //  LOG_DEBUG, "%s(%lu)\n", __func__, fuse_get_unique(req_));
+
+        fuse_reply_entry(req, e);
+        delete this;
+    }
+
+
+#if 0 // Move these methods out as and when you implement them.
+
+    virtual void replyBuf(const void* buf, size_t size) {
+        finishOperation();
+        client_->getLogger()->LOG_MSG(
+            LOG_DEBUG, "%s(%lu): %lu\n", __func__, fuse_get_unique(req_), size);
+        fuse_reply_buf(req_, (const char*)buf, size);
+        delete this;
+    }
+
+    void replyEntry(const struct fuse_entry_param* e) {
+        finishOperation();
+        client_->getLogger()->LOG_MSG(
+            LOG_DEBUG, "%s(%lu)\n", __func__, fuse_get_unique(req_));
+        fuse_reply_entry(req_, e);
+        delete this;
+    }
+
+    void replyCreate(
+        const struct fuse_entry_param* e,
+        const struct fuse_file_info* f) {
+        finishOperation();
+        client_->getLogger()->LOG_MSG(
+            LOG_DEBUG, "%s(%lu)\n", __func__, fuse_get_unique(req_));
+        fuse_reply_create(req_, e, f);
+        delete this;
+    }
+
+    void replyOpen(const struct fuse_file_info* f) {
+        finishOperation();
+        client_->getLogger()->LOG_MSG(
+            LOG_DEBUG, "%s(%lu)\n", __func__, fuse_get_unique(req_));
+        fuse_reply_open(req_, f);
+        delete this;
+    }
+
+    void replyAttr(const struct stat* attr, double attr_timeout) {
+        finishOperation();
+        client_->getLogger()->LOG_MSG(
+            LOG_DEBUG, "%s(%lu)\n", __func__, fuse_get_unique(req_));
+        fuse_reply_attr(req_, attr, attr_timeout);
+        delete this;
+    }
+
+    void replyWrite(size_t count) {
+        finishOperation();
+        client_->getLogger()->LOG_MSG(
+            LOG_DEBUG, "%s(%lu): %lu\n", __func__, fuse_get_unique(req_), count);
+        fuse_reply_write(req_, count);
+        delete this;
+    }
+
+    void replyReadlink(const char* linkname) {
+        finishOperation();
+        client_->getLogger()->LOG_MSG(
+            LOG_DEBUG, "%s(%lu): %s\n", __func__, fuse_get_unique(req_), linkname);
+        fuse_reply_readlink(req_, linkname);
+        delete this;
+    }
+
+    void replyStatfs(const struct statvfs* stbuf) {
+        finishOperation();
+        client_->getLogger()->LOG_MSG(
+            LOG_DEBUG, "%s(%lu)\n", __func__, fuse_get_unique(req_));
+        fuse_reply_statfs(req_, stbuf);
+        delete this;
+    }
+
+#endif
+
+    NfsClient* getClient() const {
+        return client;
+    }
+
+    struct fuse_req* getReq() const {
+        return req;
+    }
+};
+
+
+/// @brief base class for operations which take a parent inode and a name.
+class NfsApiContextParentName : public NfsApiContext {
+public:
+    NfsApiContextParentName(
+        NfsClient* client,
+        struct fuse_req* req,
+        enum fuse_optype optype,
+        fuse_ino_t parent,
+        const char* name)
+        : NfsApiContext(client, req, optype),
+	  parentIno(parent)
+    {
+        fileName = ::strdup(name);
+    }
+
+    ~NfsApiContextParentName() override
+    {
+        ::free((void*)fileName);
+    }
+
+    fuse_ino_t getParent() const
+    {
+        return parentIno;
+    }
+
+    const char* getName() const
+    {
+        return fileName;
+    }
+
+private:
+    fuse_ino_t parentIno;
+    const char* fileName;
+};
+
+int NfsApiContext::maxErrnoRetries(3);
---
+++ nfs/inc/nfs_client.h
@@ -0,0 +1,170 @@
+#pragma once
+
+#include "nfs_fileHandle.h"
+#include "nfs_transport.h"
+#include "nfs_internal.h"
+
+// Forward declarations for the API context classes.
+// TODO : This needs to be extended going forward for each of the nfs apis.
+class NfsApiContext;
+class NfsApiContextParentName;
+
+extern "C" {
+    // libnfs does not offer a prototype for this in any public header,
+    // but mercifully exports it anyway.
+    const struct nfs_fh3* nfs_get_rootfh(struct nfs_context* nfs);
+}
+
+//
+// This is the class which is responsible for making all the Nfsv3 API calls.
+// This is a singleton class.
+// The user should first init the class by calling NfsClient::Init() by specifying all the parameters needed to mount the filesystem.
+// Once this is done, all the callers can get an instance of this class by calling the GetInstance() method.
+// This instance can then be used to cal the APIs like getattr, write etc.
+//
+class NfsClient
+{
+private:
+
+    //
+    // This will be of the form account.blob.core.windows.net
+    // or for pre-prod : account.blob.preprod.core.windows.net
+    // or 			   : IP
+    // This will be constructed from the accountName and blobprefix passed by the caller.
+    //
+    static std::string server;
+
+    // TODO: See if we really need to store the account and container name since we already have the server and exportPath.
+    std::string accountName;
+    std::string containerName;
+
+    // Export path which is of the form /accountName/ContainerName
+    static std::string exportPath;
+
+    //
+    // Options to be passed to the mount command. Like port, proto etc.
+    //
+    struct mountOptions* mntOptions;
+
+    //
+    // File handle obtained after mounting the filesystem.
+    // This will be set after calling nfs_mount.
+    //
+    static NFSFileHandle* rootFh;
+
+    //
+    // The transport object responsible for actually sending out the requests to the server.
+    //
+    static RPCTransport* transport;
+
+    //
+    // This will be set to true if the NfsClient is init'd.
+    // This should be set to TRUE before calling the NfsClient::GetInstance()
+    //
+    static std::atomic<bool> initialized;
+
+    NfsClient(std::string* acctName, std::string* contName, std::string* blobSuffix, struct mountOptions* opt)
+    {
+        assert(acctName != nullptr);
+        assert(contName != nullptr);
+        assert(blobSuffix != nullptr);
+
+        accountName = *acctName;
+        containerName = *contName;
+
+        server = accountName + "." + *blobSuffix;
+        exportPath = "/" + accountName + "/" + containerName;
+
+        mntOptions = opt;
+    }
+
+public:
+
+    static NfsClient& GetInstanceImpl(std::string* acctName = nullptr,
+                                      std::string* contName = nullptr,
+                                      std::string* blobSuffix = nullptr,
+                                      struct mountOptions* opt = nullptr)
+    {
+        static NfsClient instance{acctName, contName, blobSuffix, opt};
+
+	// NfsClient::Init() should be called before calling this.
+        assert(IsNfsClientInitd());
+	return instance;
+    }
+
+    // This is the method which should be called to get an instance of this class by the user.
+    static NfsClient& GetInstance()
+    {
+        return GetInstanceImpl();
+    }
+
+    // The user should first init the client class before using it.
+    static bool Init(
+        std::string& acctName,
+        std::string& contName,
+        std::string& blobSuffix,
+        mountOptions* opt);
+
+    static bool IsNfsClientInitd()
+    {
+        return initialized;
+    }
+
+    //
+    // Get the nfs context on which the libnfs API calls can be made.
+    //
+    struct nfs_context* GetNfsContext()
+    {
+        return transport->GetNfsContext();
+    }
+
+    //
+    // The inode structure will be the address of the location where the actual NFS filehandle is stored.
+    // Hence by just dereferencing this structure we will be able to get the filehandle.
+    // This filehandle will remain valid till the ino is freeed by calling the free API.
+    // TODO: See when the free API should be called.
+    // /
+    NFSFileHandle* GetFhFromInode(fuse_ino_t ino)
+    {
+        if (ino == 1 /*FUSE_ROOT_ID*/)
+        {
+            return rootFh;
+        }
+        return (NFSFileHandle*)(uintptr_t)ino;
+
+    }
+
+    //
+    // Define Nfsv3 APi specific functions and helpers after this point.
+    // TODO: For now I have just added the methods needed for lookup call, add more going forward.
+    //
+    static void stat_from_fattr3(struct stat* st, const struct fattr3* attr);
+
+    virtual void replyEntry(
+        NfsApiContext* ctx,
+        const nfs_fh3* fh,
+        const struct fattr3* attr,
+        const struct fuse_file_info* file,
+        const char* caller,
+        fuse_ino_t parent,
+        const char* name);
+
+    // TODO: This should be modified to handle max retrues
+    bool shouldRetry(int rpc_status, NfsApiContext* ctx) {
+        if (rpc_status != RPC_STATUS_SUCCESS) {
+            return true;
+        }
+        return false;
+    }
+
+    void lookupWithContext(NfsApiContextParentName* ctx);
+
+    void lookup(fuse_req_t req, fuse_ino_t parent, const char* name);
+
+    static void lookupCallback(
+        struct NfsApicontext*,
+        int rpc_status,
+        void* data,
+        void* private_data);
+
+};
